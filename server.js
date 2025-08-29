8e69ab0cb9c1fe2bb8bcfecc25530dd036a04322
nunezada08 <vitor.nunes3013@gmail.com>
2025-08-29 15:40:32 -0300

    feat: adiciona rota bruxos com filro

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import dados from "./src/data/dados.js";
+const { bruxos } = dados
 
 const app = express();
 app.use(express.json());
@@ -13,6 +15,32 @@ app.get("/", (req, res) => {
 
 // Aqui vão todas suas Rotas
 
+// Query Parameters no Node.js - API de Hogwarts
+app.get('/bruxos', (req, res) => {
+    const { casa, ano, especialidade, nome } = req.query;
+    let resultado = bruxos;
+  
+    if (casa) {
+      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
+    }
+  
+    if (ano) {
+      resultado = resultado.filter(b => b.ano == ano);
+    }
+  
+    if (especialidade) {
+      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+    }
+  
+    if (nome) {
+      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+  
+    res.status(200).json({
+      total: resultado.length,
+      data: resultado
+    });
+});
 
 app.listen(serverPort, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
